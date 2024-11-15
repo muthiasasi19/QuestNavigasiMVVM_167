7f4363aab6d723ac247b811588df92b22a32cf78
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-15 17:10:43 +0700

    menambahkan val nim, email, dan noHP pada data class Mahasiswa

--- app/src/main/java/com/example/praktikumsetelahucp1/Navigasi.kt
+++ app/src/main/java/com/example/praktikumsetelahucp1/Navigasi.kt
@@ -57,7 +57,7 @@ fun Navigasi( // namanya sesuai dengan nama file
                     onBackKlick = { navHost.popBackStack() }
 
                 )
-
+            }
         }
     }
 }
--- app/src/main/java/com/example/praktikumsetelahucp1/model/Mahasiswa.kt
+++ app/src/main/java/com/example/praktikumsetelahucp1/model/Mahasiswa.kt
@@ -2,6 +2,9 @@ package com.example.praktikumsetelahucp1.model
 
 data class Mahasiswa( // data class umumnya menggunakan val
     val nama : String ="", // untuk menyimpan nama // bisa berganti tapi static (replace)
+    val nim : String = "",
+    val email : String = "",
+    val noHp : String = "",
     val gender : String ="", // untuk menyimpan gender
     val alamat : String ="" // untuk menyimpan alamat
 )
